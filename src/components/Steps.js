812dc62f184e72756936995fe70ae158e580f1aa
KevinJeanMarie <kjeanmarie01@gmail.com>
2021-10-25 16:21:52 +0200

    modif

---
+++ src/components/Steps.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Steps extends React.Component {
+    render(){
+        return (
+
+        <div class="box">
+            <div class="col-sm-3 col-6">
+                <div class="col-6">
+                    <span class="material-icons">
+                        directions_walk
+                    </span>
+                </div>
+            </div>
+        </div>
+        );
+    }
+
+}
+
+export default Steps
